6a0b865e0d75cee29ccb94291c98232aa458080d
rajanpanth <rajan.pantha@samriddhicollege.edu.np>
2025-12-30 18:50:15 +0545

    tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import React, { useState } from 'react';
+// import './App.css';
+import customcss from './customcss.module.css';
+import image from './assets/image.png';
+const Rajan = () => {
+  const [count, setCount] = useState(10);
+  return (<div>
+    <h1 className={customcss.title}>Training of react</h1>
+    <p>This is my first React component!</p>
+    <img className={customcss.myimage} src={image} alt="Walpaper" />
+    <button onClick={()=>{
+      setCount(count+1)
+    }}>Count : {count}  </button>
+  </div>)
 }
-
-export default App
+export default Rajan;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* <MyComponent /> */}
   </StrictMode>,
 )
+function MyComponent() {
+  return (<div>
+    <h1>This is my first component</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quaerat aspernatur sed error vitae ullam, quibusdam a temporibus iste repudiandae ut earum beatae laborum quae voluptates pariatur, voluptate velit officia.</p>
+  </div>)
+}
