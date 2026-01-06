488a2885cc16bf6ac1bedd4063ea18445cbbcc51
rajanpanth <rajan.pantha@samriddhicollege.edu.np>
2026-01-06 15:05:54 +0545

    Update async/await example with try-catch-finally and fetch implementation

--- javascript/day2/7.asyncAwait.js
+++ javascript/day2/7.asyncAwait.js
@@ -4,5 +4,16 @@
 // await makes a function wait for a promise
 
 let fetchUsers = async () => {
-    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        let result = await response.json();
+        console.log("user: ", result)
+    } catch (error) {
+        console.log("error is: ", error)
+    } finally {
+        console.log("this is always called")
+    }
 }
+console.log("fetching user...")
+fetchUsers();
+console.log("after user fetched")
